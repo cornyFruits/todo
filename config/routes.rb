691117a63736594195698398fb181a71b97368d4
cornyFruits <krittika.sharma07@gmail.com>
2023-05-15 23:32:26 +1000

    Added partials in todo_items and todo_lists and fixed a routes issue (resolved once code was modified in todo_lists/_form.html.erb)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
-   end  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :todo_items, only: [:create]
+  end
   root "todo_lists#index"
 end
